54fdde3d284e5f373c5b9b516978c654c93718dc
Mario Voorsluys <forcaeluz@users.noreply.github.com>
2018-07-30 21:19:03 +0200

    Add flock index view (#19)
    * Dropped the custom query in order to use djangos ORM layer.
    
    * Added the flock's index page.

--- flocks/views.py
+++ flocks/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
+from .models import Flock
+from farm.models import Farm
 
-# Create your views here.
+
+class FlockIndexView(TemplateView):
+
+    template_name = 'flocks/index.html'
+
+    def get_context_data(self, **kwargs):
+        farm = Farm.objects.get(id=self.request.session.get('farm'))
+        data = super().get_context_data(**kwargs)
+        data.update({'bread_crumbs': [farm.name, 'Flocks', 'Index']})
+        data.update({'farm': farm})
+        data.update({'current_flocks': self.get_present_flocks()})
+        data.update({'old_flocks': self.get_old_flocks()})
+        return data
+
+    def get_present_flocks(self):
+        assert (self.request.session.get('farm'))
+        farm_id = self.request.session.get('farm')
+        return Flock.objects.present().filter(farmflockrelations__farm_id=farm_id)
+
+    def get_old_flocks(self):
+        assert (self.request.session.get('farm'))
+        farm_id = self.request.session.get('farm')
+        return Flock.objects.old_flocks().filter(farmflockrelations__farm_id=farm_id)
+
