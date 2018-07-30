49313ad01f8afffdfb8f7781da79ef808e1efa06
Mario Voorsluys <forcaeluz@gmail.com>
2018-07-30 21:15:01 +0200

    Added the flock's index page.

--- farm/urls.py
+++ farm/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import FarmIndexView, NewFarmView
 from .wizards import NewAnimalEntryWizard
 from buildings.views import BuildingsIndexView
+from flocks.views import FlockIndexView
 
 
 app_name = 'farm'
@@ -10,6 +11,7 @@ urlpatterns = [
     path(r'', FarmIndexView.as_view(), name='index'),
     path(r'new_farm', NewFarmView.as_view(), name='new_farm'),
     path(r'buildings', BuildingsIndexView.as_view(), name='buildings'),
+    path(r'flocks', FlockIndexView.as_view(), name='flocks'),
     path(r'dashboard/<int:farm_id>', FarmIndexView.as_view(), name='dashboard'),
     path(r'new_animal_entry', NewAnimalEntryWizard.as_view(), name='new_animal_entry')
 ]
